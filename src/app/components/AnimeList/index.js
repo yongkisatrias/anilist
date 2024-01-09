505ec1902e88b35fcc79edaad5a69b22b53e676d
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-09 13:01:15 +0700

    [UPDATE] First setup

---
+++ src/app/components/AnimeList/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AnimeList() {
+  return <div>ANIME LIST COMPONENTS</div>;
+}
+
+export default AnimeList;
---
+++ src/app/search/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>SEARCH PAGE</div>;
+}
+
+export default Page;
