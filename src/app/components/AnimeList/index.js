4bea8c1fdafc6dfd9209bf89ad8899fe322aebc7
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-18 16:07:27 +0700

    [UPDATE] Fetch api most popular

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
-function AnimeList() {
-  return <div>ANIME LIST COMPONENTS</div>;
-}
+const AnimeList = ({ title, images }) => {
+  return (
+    <div className="bg-indigo-500">
+      <Image src={images} alt="anime" width={600} height={600} />
+      <h3>{title}</h3>
+    </div>
+  );
+};
 
 export default AnimeList;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
 import AnimeList from "./components/AnimeList";
 
-export default function Home() {
+const Home = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+  );
+  const anime = await response.json();
+
   return (
     <div>
-      <h1>AniList</h1>
-      <AnimeList />
+      <h1>Most Popular</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {anime.data.map((data) => {
+          return (
+            <AnimeList title={data.title} images={data.images.webp.image_url} />
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
