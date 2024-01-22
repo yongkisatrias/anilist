10acd8aad948aa1e5bb362bab40433479bc72d2e
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-22 12:28:33 +0700

    [UPDATE] end sessions 9 - User Dashboard Validation

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 // API
 import { authUserSession } from "@/libs/auth-libs";
@@ -7,10 +8,23 @@ const Page = async () => {
   const user = await authUserSession();
 
   return (
-    <div className="text-color-primary">
-      <h3>DASHBOARD</h3>
-      <h5>Welcome, {user?.name}</h5>
+    <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+      <h5 className="text-2xl font-bold mb-2">Welcome, {user?.name}</h5>
       <Image src={user?.image} alt="profile picture" width={250} height={250} />
+      <div className="py-8 flex flex-wrap gap-4">
+        <Link
+          href="/users/dashboard/collection"
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Collection
+        </Link>
+        <Link
+          href="/users/dashboard/comment"
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Comment
+        </Link>
+      </div>
     </div>
   );
 };
